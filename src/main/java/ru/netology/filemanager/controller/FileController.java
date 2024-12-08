9f74f375bfdd7dae47184e1979971871cb145674
Vitaliy Murzin <murzinvit@yandex.ru>
2024-12-08 23:07:49 +0500

    add controller

---
+++ src/main/java/ru/netology/filemanager/controller/FileController.java
@@ -0,0 +1,30 @@
+package ru.netology.filemanager.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import ru.netology.filemanager.model.FileInfo;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/file")
+@RequiredArgsConstructor
+public class FileController {
+
+    private final FileService fileService;
+
+    @PostMapping
+    public ResponseEntity<FileInfo> upload(@RequestParam MultipartFile attachment) {
+        try {
+            return new ResponseEntity<>(fileService.upload(attachment), HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
